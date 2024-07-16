3df820185e62e89d10a827e4abf85b1e9d823363
marduk191 <marduk191@gmail.com>
2024-07-16 00:19:56 -0500

    added placeholder node for installing through manager in workflows

--- __init__.py
+++ __init__.py
@@ -1,6 +1,29 @@
+import __main__
 
+class Gifloader_Placeholder:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return { }
+
+    RETURN_TYPES = ()
+    FUNCTION = "gifloadrr"
+
+    CATEGORY = "marduk191/GifLoader"
+
+    def placeholder(self):
+        return ()
+    
 NODE_CLASS_MAPPINGS = {
-    # Add mappings here
-    }
+    "Gifloader_Placeholder": Gifloader_Placeholder
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Gifloader_Placeholder": "marduk191's Gifloader",
+}
 
 WEB_DIRECTORY = "./web"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
